519790a5a0eaa0158ec4d1fec505bc74e0b922a6
yuchi0731 <kireha2180@gmail.com>
2021-07-30 13:55:56 +0800

    get model

--- AccountingNote_Auth/AuthManager.cs
+++ AccountingNote_Auth/AuthManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using AccountingNote_DBSoure;
 
 namespace AccountingNote_Auth
 {
@@ -15,11 +18,38 @@ namespace AccountingNote_Auth
         public static bool IsLogined()
         {
 
-            if (System.Web.HttpContext.Current.Session["UserLoginInfo"] == null)
+            if (HttpContext.Current.Session["UserLoginInfo"] == null)
                 return false;
             else
-                return true;
-            
+                return true;           
         }
+
+        public static UserInfoModel GetCurrentUser()
+        {
+            string account = HttpContext.Current.Session["UserLoginInfo"] as string;
+
+            if (account == null)
+                return null;
+
+            DataRow dr = UserInfoManager.GetUserInfoByAccount(account);
+            
+
+            if (dr == null)
+                return null;
+            //因為dr設為取得使用者帳號的方法，所以可以取得對應的欄位
+            UserInfoModel model = new UserInfoModel();
+            model.ID = dr["ID"].ToString();
+            model.Account = dr["Account"].ToString();
+            model.Name = dr["Name"].ToString();
+            model.Email = dr["Email"].ToString();
+
+
+            return model;
+
+
+        }
+
+
+
     }
 }
---
+++ AccountingNote_Auth/UserInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote_Auth
+{
+    public class UserInfoModel
+    {
+        //和DB裡的欄位一致
+        public string ID { get; set; }
+        public string Account { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
