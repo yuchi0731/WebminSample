3a0509b2fbee54a49a3ddcf9b1741232335a210e
yuchi0731 <kireha2180@gmail.com>
2021-07-30 14:48:54 +0800

    (重構) 登入登出確認

--- AccountingNote_Auth/AuthManager.cs
+++ AccountingNote_Auth/AuthManager.cs
@@ -21,7 +21,7 @@ namespace AccountingNote_Auth
             if (HttpContext.Current.Session["UserLoginInfo"] == null)
                 return false;
             else
-                return true;           
+                return true;
         }
 
         /// <summary>
@@ -36,14 +36,14 @@ namespace AccountingNote_Auth
                 return null;
 
             DataRow dr = UserInfoManager.GetUserInfoByAccount(account);
-            
+
 
             if (dr == null) //一旦發現是空的就要清除資料
             {
                 HttpContext.Current.Session["UserLoginInfo"] = null;
                 return null;
             }
-                
+
             //因為dr設為取得使用者帳號的方法，所以可以取得對應的欄位
             UserInfoModel model = new UserInfoModel();
             model.ID = dr["ID"].ToString();
@@ -56,13 +56,54 @@ namespace AccountingNote_Auth
 
         }
         /// <summary>
-        /// 清除登入
+        /// 登出
         /// </summary>
         public static void Logout()
         {
             HttpContext.Current.Session["UserLoginInfo"] = null; //清除登入資料，導向登入頁
         }
 
+        /// <summary>
+        /// 嘗試登入
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="pwd"></param>
+        /// <param name="errorMsg"></param>
+        /// <returns></returns>
+        public static bool TryLogin(string account, string pwd, out string errorMsg)
+        {
+            //check empty
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(pwd))
+            {
+                errorMsg = "Account / PWD is required.";
+                return false;
+            }
+
+            //read db and check
+            var dr = UserInfoManager.GetUserInfoByAccount(account);
+
+            //check null
+            if (dr == null)
+            {
+                errorMsg = $"Account:{account} doesn't existi.";
+                return false;
+            }
+
+            //check account / pwd  Compare:比對前兩數值，第三參數為true不分大小寫，false表示區分大小寫
+            if (string.Compare(dr["Account"].ToString(), account, true) == 0 && string.Compare(dr["PWD"].ToString(), pwd, false) == 0)
+            {
+                HttpContext.Current.Session["UserLoginInfo"] = dr["Account"].ToString();
+                errorMsg = string.Empty;
+                return true;
+            }
+
+            else
+            {
+                errorMsg = "Login fail. Please check Account / PWD.";
+                return false;
+            }
+
+        }
 
 
     }
--- WebApplication1/Login.aspx.cs
+++ WebApplication1/Login.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using AccountingNote_DBSoure;
+using AccountingNote_Auth;
 
 namespace _0728_1
 {
@@ -27,35 +28,51 @@ namespace _0728_1
             string inp_Account = this.txtAccount.Text;
             string inp_PWD = this.txtPWD.Text;
 
-            //check empty
-            if (string.IsNullOrWhiteSpace(inp_Account) || string.IsNullOrWhiteSpace(inp_PWD))
+            string msg;
+
+            if(!AuthManager.TryLogin(inp_Account, inp_PWD, out msg))
             {
-                this.ltlMsg.Text = "Account / PWD is required.";
+                this.ltlMsg.Text = msg;
                 return;
-            }
-
-            var dr = UserInfoManager.GetUserInfoByAccount(inp_Account);
-
-            //check null
-            if (dr == null)
-            {
-                this.ltlMsg.Text = "Account doesn't existi.";
-                return;
-            }
-
-
-            //check account / pwd  Compare:比對前兩數值，第三參數為true不分大小寫，false表示區分大小寫
-            if (string.Compare(dr["Account"].ToString(), inp_Account, true) == 0 && string.Compare(dr["PWD"].ToString(), inp_PWD, false) == 0)
-            {
-                this.Session["UserLoginInfo"] = dr["Account"].ToString();
-                Response.Redirect("/SystemAdmin/UserInfo.aspx");
             }
 
             else
             {
-                this.ltlMsg.Text = "Login fail. Please check Account / PWD.";
-                return;
+                Response.Redirect("/SystemAdmin/UserInfo.aspx");
             }
+
+
+
+
+            ////check empty
+            //if (string.IsNullOrWhiteSpace(inp_Account) || string.IsNullOrWhiteSpace(inp_PWD))
+            //{
+            //    this.ltlMsg.Text = "Account / PWD is required.";
+            //    return;
+            //}
+
+            //var dr = UserInfoManager.GetUserInfoByAccount(inp_Account);
+
+            ////check null
+            //if (dr == null)
+            //{
+            //    this.ltlMsg.Text = "Account doesn't existi.";
+            //    return;
+            //}
+
+
+            ////check account / pwd  Compare:比對前兩數值，第三參數為true不分大小寫，false表示區分大小寫
+            //if (string.Compare(dr["Account"].ToString(), inp_Account, true) == 0 && string.Compare(dr["PWD"].ToString(), inp_PWD, false) == 0)
+            //{
+            //    this.Session["UserLoginInfo"] = dr["Account"].ToString();
+            //    Response.Redirect("/SystemAdmin/UserInfo.aspx");
+            //}
+
+            //else
+            //{
+            //    this.ltlMsg.Text = "Login fail. Please check Account / PWD.";
+            //    return;
+            //}
 
         }
     }
